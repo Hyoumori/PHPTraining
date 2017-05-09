872daf0f73f13c6cc0fade175e7f89180b31f633
Hyoumori <xx_final_order@yahoo.co.jp>
2017-05-09 11:32:42 +0900

    定数と文字列と繰り返し

---
+++ 20170509/定数　課題１.php
@@ -0,0 +1,9 @@
+<?php
+define("CONSTANT1","Hello PHP");
+//echoなら()はいらない。
+//printだったら()がいる。
+echo CONSTANT1 ;
+echo "<br>" ;
+const CONSTANT2 ="Hello PHP 5.3" ;
+echo CONSTANT2 ;
+?>
---
+++ 20170509/文字列　課題１.php
@@ -0,0 +1,9 @@
+<?php
+$A = "Hello world!";
+$a = "Hello kitty!";
+echo $A ;
+echo "<br>";
+echo $a ;
+echo "<br>";
+echo str_replace("world","kitty",$A);
+?>
