036d8c7518ac3f3e776d2afdc4c5a74909495da5
Hyoumori <xx_final_order@yahoo.co.jp>
2017-05-25 17:56:23 +0900

    20170525_Web

--- 20170522/Web01.php
+++ 20170522/Web01.php
@@ -7,7 +7,7 @@
         <hr>
         <title>お問い合わせ</title>
     </head>
-    <body text="A9085B">
+    <body text="orange">
         <link rel="stylesheet" type="text/css" href="Web_design.css"/>
 
             <form action="Web02.php" method="post" name="form" onsubmit="return validate()">
@@ -17,10 +17,10 @@
                 <br>
                 <table border="0">
                 <tr><td>
-                    姓   　　　　　　　　　<input type="text" name="last_name" size=2><br>
+                    姓   　　　　　　　　　<input required type="text" name="last_name" size=2 pattern="[^\s]+" placeholder="毛利"><br>
                 </td></tr>
                 <tr><td>
-                    名   　　　　　　　　　<input type="text" name="first_name" size=2><br>
+                    名   　　　　　　　　　<input required type="text" name="first_name" size=2 pattern="[^\s]+" placeholder="小五郎"><br>
                 </td></tr>
                 <tr><td>
                     性別  　　　　　　　　<input type="radio" name="gender" value="男">男
@@ -28,30 +28,50 @@
                         <input type="radio" name="gender" value="不明" checked="checked">不明<br>
                     </td></tr>
                     <tr><td>
-                    住所  　　　　　　　　<input type="text" name="address" size="2"><br>
+                    住所  　　　　　　　　<input type="text" name="address" size="19"><br>
                 </td></tr>
                 <tr><td>
-                    電話番号    　　　　　　<input type="tel" name="phone01" size="1">
-                    -   <input type="tel" name="phone02" size="1">
-                    -   <input type="tel" name="phone03" size="2">
+                    電話番号    　　　　　　<input required type="tel" name="phone01" size="1" pattern="^[0-9]+$">
+                    -   <input required type="tel" name="phone02" size="1" pattern="^[0-9]+$">
+                    -   <input required type="tel" name="phone03" size="2" pattern="^[0-9]+$">
                     <br>
                 </td></tr>
                 <tr><td>
-                    メールアドレス 　　　<input type="text" name="mail01" size="5">
-                    @   <input type="text" name="mail02" size="5"><br>
+                    メールアドレス 　　　<input required type="text" name="mail01" size="5" pattern="[^\s]+@[^\s]+"><br>
+                    <!--@   <input type="text" name="mail02" size="5"><br>-->
                 </td></tr>
                 <tr><td>
-                    <!--<input type="hidden" name="where" value="none">-->
+                    <input type="hidden" name="where01" value="">
+                    <input type="hidden" name="where02" value="">
                     どこで知ったか     　　　<input type="checkbox" name="where01" value="雑誌">雑誌
                                         <input type="checkbox" name="where02" value="新聞">新聞<br>
                 </td></tr>
                 <tr><td>
-                    質問カテゴリ  　　　　<input type="text" name="category" size="2"><br>
+                    質問カテゴリ  　　　　<select name="category">
+                                        <!--<input type="text" name="category" size="2"><br>-->
+                                        <option value="---">----</option>
+                                        <option value="Yes1">Yes</option>
+                                        <option value="Yes2">Yes</option>
+                                        <option value="Yes3">Yes</option>
+                                        <option value="Yes4">Yes</option>
+                                        <option value="Yes5">Yes</option>
+                                        <option value="Yes6">Yes</option>
+                                        <option value="Yes7">Yes</option>
+                                        <option value="Yes8">Yes</option>
+                                        <option value="Yes9">Yes</option>
+                                        <option value="Yes10">Yes</option>
+                                        <option value="Yes11">Yes</option>
+                                        <option value="Yes12">Yes</option>
+                                        <option value="Yes13">Yes</option>
+                                        <option value="Yes14">Yes</option>
+                                        <option value="Yes15">Yes</option>
+                                        </select>
+                    <br>
                     <br>
                 </td></tr>
                 <tr><td>
                     質問内容<br>
-                    <textarea name="question" cols="44" rows="8"></textarea>
+                    <textarea name="question" cols="44" rows="8" ></textarea>
                 </td></tr>
                 <tr><td>
                     <input type="submit" name="submit" value="確認">
--- 20170522/Web03.php
+++ 20170522/Web03.php
@@ -1,53 +1,79 @@
+
+今回は使用しない!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+<!DOCTYPE html>
 <html>
     <head>
-        <?php
-            $page = $_POST['last_name'] === "" or $_POST['first_name'] === "" or $_POST['phone'] === "" or $_POST['mail'] === "";
-            if($page){
-                print "<title>お問い合わせエラー</title>";
-            }else{
-                print "<title>お問い合わせ終了</title>";
-            }
+        <!--
+            問合せ内容確認
 
-        ?>
-
+            送信 → 問合せ完了：Web04.php
+            戻る → (入力内容を初期化せずに)問合せフォーム Web01.php
+            未入力状態で書き直します → (入力内容を初期化して)問合せフォーム Web01.php
+        -->
+        <title>確認</title>
+        <link rel="stylesheet" type="text/css" href="Web_design.css">
     </head>
 
     <body>
         <center>
-            <?php
-                if(!$page){
-                    print "お問い合わせ終了<br><hr>";
-                    print "姓：<br>".$_POST['last_name']."<br><br>";
-                    print "名：<br>".$_POST['first_name']."<br><br>";
-                    print "性別：<br>".$_POST['gender']."<br><br>";
-                    print "住所：<br>".$_POST['address']."<br><br>";
-                    print "電話番号：<br>".$_POST['phone']."<br><br>";
-                    print "メールアドレス：<br>".$_POST['mail']."<br><br>";
-                    print "どこで知ったか：<br>".$_POST['where']."<br><br>";
-                    print "質問カテゴリ：<br>".$_POST['category']."<br><br>";
-                    print "質問内容：<br>".nl2br($_POST['question'])."<br><br>";
-                }
-                $fp = fopen( "count.csv", "r+" );
-                //$str=trim(fgets($fp));
-                $count = -1;
-                while(($tmp=fgets($fp))!=false){
-                    $str = $tmp;
-                    $str = trim($str);
-                    $count = explode(",",$str)[0];
-                }
-                $count++;
-                $output = $count;
-                foreach($_POST as $key => $p){
-                    if($key != "submit" and $p != "送信"){
-                        $output .= ",".$p;
+            <!--<form method="post" action="Web04.php">-->
+
+
+                    <!--問い合わせ内容の確認-->
+                    <hr>
+                    <h1>お問い合わせ内容</h1>
+                    姓：<br><?= $_POST['last_name']?><br><br>
+                    名：<br><?= $_POST['first_name']?><br><br>
+                    性別：<br><?= $_POST['gender']?><br><br>
+                    住所：<br><?= $_POST['address']?><br><br>
+                    電話番号：<br><?= $_POST['phone01']."-".$_POST['phone02']."-".$_POST['phone03']?><br><br>
+                    メールアドレス：<br><?= $_POST['mail']?><br><br>
+                    どこで知ったか：<br>
+                <?php
+                    if(isset($_POST['where'])){
+                        foreach($_POST['where'] as $w){
+                            print $w."<br>";
+                        }
+                    }else{
+                        print "<br>";
                     }
-                }
-                fputs( $fp, $output."\n");
-                fclose( $fp );
-                print "<hr>";
-                print "お問い合わせ番号：".$count."<br>";
-            ?>
-            <a href="test.php">未入力状態で書き直します</a>
+                ?>
+                    <br>
+                    質問カテゴリ：<br><?= $_POST['category']?><br><br>
+                    質問内容：<br><?= $_POST['question']?><br><br>
+
+                    <!--送信された場合のデータ送信用。-->
+                    <input type="hidden" name="last_name" value=<?= $_POST['last_name']?>>
+                    <input type="hidden" name="first_name" value=<?= $_POST['first_name']?>>
+                    <input type="hidden" name="gender" value=<?= $_POST['gender']?>>
+                    <input type="hidden" name="address" value=<?= $_POST['address']?>>
+                    <input type="hidden" name="phone" value=<?= $_POST['phone01']."-".$_POST['phone02']."-".$_POST['phone03']?>>
+                    <input type="hidden" name="mail" value=<?= $_POST['mail']?>>
+                <?php
+                    if(isset($_POST['where'])){
+                        foreach($_POST['where'] as $w){
+                            print "<input type=\"hidden\" name=\"where[]\" value=".$w.">";
+                        }
+                    }else{
+                        print "<input type=\"hidden\" name=\"where[]\" value=\"\">";
+                    }
+                ?>
+                    <input type="hidden" name="category" value=<?= $_POST['category']?>>
+                <?php
+                    foreach(explode("\n",$_POST['question']) as $line){
+                        print "<input type=\"hidden\" name=\"question[]\" value=".$line.">";
+                    }
+                    print $_POST['question'];
+                ?>
+                <input type="submit" name="submit" value="送信">
+                <!--戻る(入力された状態)-->
+                <input type="button" value="戻る" onclick="history.back()">
+            </form>
+
+            <hr>
+            <!--戻る(未入力状態)-->
+            <a href="web01.php">未入力状態で書き直します</a>
         <center>
     </body>
 </html>
